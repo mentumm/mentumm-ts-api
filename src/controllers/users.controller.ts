5406c4d58bebdb4037213029fc3e904ef568488a
Ryan V <47340962+denvermullets@users.noreply.github.com>
2022-04-29 09:10:34 -0400

    feat(users): add users routes, g10-8 (#4)

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,80 @@
 import { Request, Response } from "express";
+import { CreateUser } from "../models/users.model";
+import {
+  createUser,
+  deleteUser,
+  getUsers,
+  updateUser,
+} from "../services/users.service";
 
 export const users = async (req: Request, res: Response) => {
-  // do stuff
-  console.log("hi buddy");
+  const { id, name, email, employer_id } = req.query;
+
+  if (!id && !name && !email && !employer_id) {
+    return res.status(400).send("Missing required properties");
+  }
+
+  try {
+    const user = await getUsers(
+      Number(id),
+      name as string,
+      email as string,
+      Number(employer_id)
+    );
+
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+export const newUser = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  // technically you can create a User with a deactivated Employer acct
+  // we will handle this on the front-end but good to remember if API is opened up
+  const body: CreateUser = req.body;
+
+  if (!body || !body.name || !body.email || !body.employer_id) {
+    return res.status(400).send("Missing required body properties");
+  }
+
+  try {
+    const user = await createUser(body);
+
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+export const deactivateUser = async (req: Request, res: Response) => {
+  const id = req.query.id;
+
+  try {
+    if (!id) {
+      return res.status(400).send("Missing required parameters");
+    }
+
+    const user = await deleteUser(Number(id));
+
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+};
+
+export const updateUserInfo = async (req: Request, res: Response) => {
+  if (!req.body.id) {
+    return res.status(400).send("Missing User ID");
+  }
+
+  try {
+    const updatedUser = await updateUser(req.body);
+
+    return res.json(updatedUser);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 };
