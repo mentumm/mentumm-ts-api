11a05f3ced9dda851222a0d659a2c1a80c3db9f8
denvermullets <devmullets@gmail.com>
2022-05-09 08:32:49 -0400

    feat(tags): tags db migration

---
+++ src/database/migrations/20220509122516_add_description_to_tags.ts
@@ -0,0 +1,13 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return await knex.schema.alterTable("tags", (t) => {
+    t.string("description").notNullable();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return await knex.schema.alterTable("tags", (t) => {
+    t.dropColumn("description");
+  });
+}
