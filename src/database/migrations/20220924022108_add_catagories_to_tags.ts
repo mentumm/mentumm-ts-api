737f66ab78719e995b2c5d93e782c552b776a184
jaimemasson <jaime.masson@picmonic.com>
2022-10-04 17:25:03 -0600

    feat(tags): add category column to tag (#71)

---
+++ src/database/migrations/20220924022108_add_catagories_to_tags.ts
@@ -0,0 +1,76 @@
+import { Knex } from "knex";
+import db from "../db";
+
+
+export async function up(knex: Knex): Promise<void> {
+  await db("tags")
+    .whereIn('slug', [
+      'career-planning',
+      'communication',
+      'content-development',
+      'customer-care',
+      'linkedin-marketing',
+      'negotiation',
+      'networking',
+      'presentation-public-speaking',
+      'sales',
+      'time-management-productivity',
+    ])
+    .update({ category: 'Professional' })
+    .catch((err: Error) => {
+      console.log(err);
+      return { message: "Unable to update Tags" };
+    });
+
+    await db("tags")
+    .whereIn('slug', [
+      'collaboration',
+      'conflict-management',
+      'decision-making',
+      'interview-skills',
+      'leadership',
+      'management',
+      'mentoring',
+      'onboarding-training',
+      'problem-solving',
+      'relationship-management',
+      'strategic-planning',
+      'workplace-culture',
+    ])
+    .update({ category: 'Leadership' })
+    .catch((err: Error) => {
+      console.log(err);
+      return { message: "Unable to update Tags" };
+    });
+
+    await db("tags")
+    .whereIn('slug', [
+      'goal-setting',
+      'life-coaching',
+      'meditation',
+      'nutrition',
+      'parenting',
+      'personal-branding',
+      'personal-finance',
+      'personal-fitness',
+      'sleep',
+      'stress-management',
+      'work-life-balance',
+    ])
+    .update({ category: 'Personal' })
+    .catch((err: Error) => {
+      console.log(err);
+      return { message: "Unable to update Tags" };
+    });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await db("tags")
+    .update({ category: null })
+    .catch((err: Error) => {
+      console.log(err);
+      return { message: "Unable to update Tags" };
+    });
+}
+
