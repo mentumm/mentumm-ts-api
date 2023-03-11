1009e3feb7b55d55c878d806489bf388404e9406
Eric Stout <ericwstout@gmail.com>
2023-03-10 20:28:52 -0600

    Add helmet

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,19 @@ import express, { Express, Request, Response } from "express";
 import cors, { CorsRequest } from "cors";
 import passport from "passport";
 import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
-import jwt from "jsonwebtoken";
+import helmet from "helmet";
 
 import usersRouter from "./routes/users.routes";
 import employersRouter from "./routes/employers.routes";
 import coachesRouter from "./routes/coaches.routes";
 import tagsRouter from "./routes/tags.routes";
 import { tokenRouter } from "./routes/tokens.routes";
-import { jwtOptions } from "./util/jwtOptions";
 
 const app: Express = express();
+app.use(helmet());
 
 const port = process.env.PORT || 3001;
 const JWT_SECRET = process.env.JWT_SECRET || "";
-
-console.log(JWT_SECRET);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
