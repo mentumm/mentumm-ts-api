4736121b12848b5021a152ca63f5e72e6d66ea16
Neal Lambert <crankeye@gmail.com>
2023-03-15 23:27:47 -0700

    this should properly work for pr envs now

--- src/util/corsOptions.ts
+++ src/util/corsOptions.ts
@@ -1,11 +1,13 @@
 import { CorsOptions } from "cors";
 
 const NODE_ENV = process.env.NODE_ENV;
+const RENDER_EXTERNAL_URL = process.env.RENDER_EXTERNAL_URL ?? false;
 
 const privateWhitelist = [
   "https://*.mentumm.com",
   "https://mentumm.com",
-  "https://*.onrender.com",
+  "https://mentumm-portal-staging.onrender.com",
+  NODE_ENV === "staging" && RENDER_EXTERNAL_URL,
   NODE_ENV === "development" && "http://localhost:3000",
 ];
 
