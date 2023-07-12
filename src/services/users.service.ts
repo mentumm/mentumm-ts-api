6d4507393bf286e9784bbfd1e69b585bc8dc7316
Neal Lambert <crankeye@gmail.com>
2023-07-11 21:59:24 -0700

    fix(user-service): Allow emails in staging to point to correct render url

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -421,7 +421,9 @@ export const initiatePasswordReset = async (email: string): Promise<void> => {
 
     emailService.send(EmailTemplate.PASSWORD_RESET, user.email, {
       first_name: user.first_name,
-      password_reset_link: `${process.env.BASE_URL}/reset-password/${token}`,
+      password_reset_link: `${
+        process.env.RENDER_EXTERNAL_URL ?? process.env.BASE_URL
+      }/reset-password/${token}`,
     });
   }
 };
