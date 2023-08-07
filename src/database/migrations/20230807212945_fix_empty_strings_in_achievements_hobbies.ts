a3e8f256924a4087a11e2c82583d1ba610ebd94b
Ryan McCabe <ryan.mccabe089@gmail.com>
2023-08-07 14:58:54 -0700

    created migration to remove empty string entries for achievements/hobbies

---
+++ src/database/migrations/20230807212945_fix_empty_strings_in_achievements_hobbies.ts
@@ -0,0 +1,30 @@
+import { Knex } from "knex";
+import { User } from "../../models/users.model";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.transaction(async trx => {
+    const users: User[] = await trx('users').select('id', 'achievements', 'hobbies');
+
+    for (const user of users) {
+      const achievementsArray = JSON.parse(user.achievements);
+      const hobbiesArray = JSON.parse(user.hobbies);
+
+      const filteredAchievements = achievementsArray.filter((item: string) => item.trim() !== "");
+      const filteredHobbies = hobbiesArray.filter((item: string) => item.trim() !== "");
+
+      await trx('users')
+        .where('id', user.id)
+        .update({
+          achievements: JSON.stringify(filteredAchievements),
+          hobbies: JSON.stringify(filteredHobbies),
+        });
+    }
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  // rollback not recommended, leaving this empty.
+  return Promise.resolve();
+}
+
